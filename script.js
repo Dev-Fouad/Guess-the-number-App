37f6ff35a8567448080b79355c54e7a83a27c7c5
fouad ajayi <fouadajayi6@gmail.com>
2022-10-27 14:53:15 +0100

    Made some changes

---
+++ script.js
@@ -0,0 +1,11 @@
+'Use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '😍 Correct number';
+
+// document.querySelector('.number').textContent = 13
+// document.querySelector('.score').textContent= 10
+
+// document.querySelector('.guess').value = 23;         
+// console.log(document.querySelector('.guess').value); 
